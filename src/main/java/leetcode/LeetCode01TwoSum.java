34e481568109e5c554e0968a0fbe31a56a30d236
zhailz <zhailz@chanjet.com>
2016-04-12 20:50:14 +0800

    leetcode--01

---
+++ src/main/java/leetcode/LeetCode01TwoSum.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @author zhailz
+ *
+ * 时间：2016年4月12日 ### 下午8:46:34
+ */
+public class LeetCode01TwoSum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
